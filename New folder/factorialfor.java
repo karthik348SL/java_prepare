acf8934a6853d262f8490fa2b7bd49587b73a859
karthik348SL <karthiksl348@gmail.com>
2025-09-20 19:43:20 +0530

    factorialfor.java

--- New folder/factorialfor.java
+++ New folder/factorialfor.java
@@ -1,17 +1,19 @@
 import java.util.*;
 
-public class factorialfor{
+public class reversenum{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter number for factorial ");
+        System.out.println("Enter how much number for the reverse number ");
         int a = sc.nextInt();
-        int next = 1;
+        System.out.println("Enter the number for reverse number ");
+        int b = sc.nextInt();
         
-        for(int i = 1;i<=a;i++){
-            int term = next * i;
-            next = term;
-            System.out.println("the factorial of the given number is "+term);
-            
+        for(int i = 1; i<=a; i++){
+            int r = b %10;
+            int q = b/10;
+            b = q;
+            System.out.println("the reverse number is "+ r);
         }
+        
     }
 }
