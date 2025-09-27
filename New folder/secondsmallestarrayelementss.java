294710c427a36042a405fcd5e6417181351bb5b1
Karthik S L <karthiksl348@gmail.com>
2025-09-28 00:15:06 +0530

    Add files via upload
    code which gives second smallest element in array

---
+++ New folder/secondsmallestarrayelementss.java
@@ -0,0 +1,28 @@
+public class secondsmallestarrayelementss {
+    public static void main(String[] args){
+        int max1 = 0;
+        int max2 = 0;
+        int a[] = {3,9,7,8,12,6,5,15,4,10};
+
+        if(a[0]<a[1]){
+            max1 = a[0];
+            max2 = a[1];
+        }
+        else{
+            max1 = a[1];
+            max2 = a[0];
+        }
+        for(int i = 2;i<a.length;i++){
+            if(a[i]<max1){
+                max2 = max1;
+                max1 = a[i];
+
+            }
+            else if(a[i]<max2){
+                max2 = a[i];
+            }
+        }
+        System.out.println("the smallest number is "+max1);
+        System.out.println("the second smallest number is "+max2);
+    }
+}
