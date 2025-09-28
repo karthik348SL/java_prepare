67bfaed2486c9bb643f3ed916d54cf1cb54802e3
Karthik S L <karthiksl348@gmail.com>
2025-09-28 10:25:30 +0530

    Add files via upload
    it is used to find the largest and smallest number in array element 
    
    to run :
     just copy and paste the code in vs code or even with java online compiler

---
+++ New folder/largestandsmallestofarrayelement.java
@@ -0,0 +1,27 @@
+public class largestandsmallestofarrayelement {
+    public static void main(String[] args){
+    int a[] = {3, 9, 7, 8, 12, 6, 5, 15, 4, 10};
+    int max1 = 0;
+    int max2 = 0;
+
+    if(a[0]>a[1]){
+        max1 = a[0];
+        max2 = a[1];
+    }
+    else{
+        max1 = a[1];
+        max2 = a[0];
+    }
+
+    for(int i = 2;i<a.length;i++){
+        if(a[i]>max1){
+            max1 = a[i];
+        }
+        if(a[i]<max2){
+            max2 = a[i];
+        }
+    }
+    System.out.println("the largest number is "+max1);
+    System.out.println("the smallest number is "+max2);
+    }
+}
